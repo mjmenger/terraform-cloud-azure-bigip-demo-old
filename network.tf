a7cba36f877bc358c628261015fed3726d6c514b
Mark Menger <m.menger@f5.com>
2020-04-02 22:18:59 -0700

    add a bastion subnet

--- network.tf
+++ network.tf
@@ -16,6 +16,15 @@ resource "azurerm_virtual_network" "main" {
 }
 
 # Create management subnet
+resource "azurerm_subnet" "bastion" {
+    name                 = "AzureBastionSubnet"
+    resource_group_name  = azurerm_resource_group.main.name
+    virtual_network_name = azurerm_virtual_network.main.name
+    # address prefix 10.1x.0.0/24
+    address_prefix       = "10.40.0.0/24"
+}
+
+# Create management subnet
 resource "azurerm_subnet" "management" {
     count                = length(local.azs)
     name                 = format("%s-managementsubnet-%s-%s",var.prefix,count.index,random_id.randomId.hex)
