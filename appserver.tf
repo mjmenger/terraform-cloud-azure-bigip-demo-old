aef8369835d1a751cf72384ed697e680441810a5
Mark Menger <m.menger@f5.com>
2020-04-08 08:15:41 -0700

    remove references to transfer resource

--- appserver.tf
+++ appserver.tf
@@ -163,8 +163,7 @@ resource "null_resource" "virtualserverAS3" {
 
   depends_on = [
     azurerm_linux_virtual_machine.f5bigip,
-    azurerm_virtual_machine_extension.run_startup_cmd,
-    null_resource.transfer
+    azurerm_virtual_machine_extension.run_startup_cmd
   ]
 }
 
