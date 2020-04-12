20a6ebb213c96a58a596733fece7ef2efa084166
Mark Menger <m.menger@f5.com>
2020-04-12 19:52:30 +0000

    variable declarations

--- variables.tf
+++ variables.tf
@@ -82,3 +82,8 @@ variable onboard_log {
   default     = "/var/log/startup-script.log"
 }
 
+variable "ARM_CLIENT_ID" {}
+variable "ARM_CLIENT_SECRET" {}
+variable "ARM_SUBSCRIPTION_ID" {}
+variable "ARM_TENANT_ID" {}
+
